e164dbda4ddc66a198854dcf4ed6e3bc93c8ba0a
stev <stefano.valladares@akamas.io>
2022-01-01 21:52:18 +0100

    Fix user-dirs

--- bin/build-arch/set-dirs.sh
+++ bin/build-arch/set-dirs.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env zsh
 
-mkdir -p desktop
-mkdir -p documents
-mkdir -p media
-mkdir -p media/music
-mkdir -p media/videos
-mkdir -p media/pics
-mkdir -p downloads
-mkdir -p documents/templates
-mkdir -p code
-mkdir -p notes
-mkdir -p work
-mkdir -p packages
-mkdir -p public
+mkdir -p ~/desktop
+mkdir -p ~/documents
+mkdir -p ~/media
+mkdir -p ~/media/music
+mkdir -p ~/media/videos
+mkdir -p ~/media/pics
+mkdir -p ~/downloads
+mkdir -p ~/documents/templates
+mkdir -p ~/code
+mkdir -p ~/notes
+mkdir -p ~/work
+mkdir -p ~/packages
+mkdir -p ~/public
 
-rm -rf Desktop Documents Music Downloads Pictures Public Videos Templates
+rm -rf ~/Desktop ~/Documents ~/Music ~/Downloads ~/Pictures ~/Public ~/Videos ~/Templates
