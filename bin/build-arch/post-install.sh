eba609ca00e1a05630805728fd145bb1fb48cf3c
stev <stefano.valladares@akamas.io>
2022-01-01 23:42:03 +0100

    Add jetbrains

--- bin/build-arch/post-install.sh
+++ bin/build-arch/post-install.sh
@@ -132,7 +132,7 @@ bash "$ROOT_DIR"/bin/build-arch/add-cronjobs.sh
 ## SWITCHING USER #
 ###################
 echo "Login with $username user"
-su "$username" <<EOF
+su "$username" --preserve-environment <<EOF
 
   ROOT_DIR=/root/bayo-arch
 
